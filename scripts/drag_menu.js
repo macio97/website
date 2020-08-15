0dc6fb2f5e9298913a7564e8eb68113e15d81bd1
Nathan Craddock <nzcraddock@gmail.com>
2020-08-15 09:24:05 -0600

    Fix dragmenu js

--- scripts/drag_menu.js
+++ scripts/drag_menu.js
@@ -1,46 +1,49 @@
-const slider = document.querySelector('#submenu');
-let isDown = false;
-let startX;
-let scrollLeft;
+window.onload = function() {
+	const slider = document.querySelector('#submenu');
+	let isDown = false;
+	let startX;
+	let scrollLeft;
 
-var clickHandler = function (event) {
-	event.preventDefault();
-	}
-
-elements = document.getElementById("submenu")
-elements = elements.querySelectorAll("a")
-
-slider.addEventListener('mousedown', (e) => {
-	isDown = true;
-	isDragging = false;
-	startX = e.pageX - slider.offsetLeft;
-	scrollLeft = slider.scrollLeft;
-	for (var i = 0; i < elements.length; i++) {
-		elements[i].addEventListener("click", clickHandler, false);
+	var clickHandler = function (event) {
+		event.preventDefault();
 		}
-	});
 
-slider.addEventListener('mouseleave', () => {
-	isDown = false;
-	});
+	elements = document.getElementById("submenu")
+	elements = elements.querySelectorAll("a")
 
-slider.addEventListener('mouseup', () => {
-	if(isDragging==true){
+	slider.addEventListener('mousedown', (e) => {
+		isDown = true;
 		isDragging = false;
-		}
-	else {
+		startX = e.pageX - slider.offsetLeft;
+		scrollLeft = slider.scrollLeft;
 		for (var i = 0; i < elements.length; i++) {
-			elements[i].removeEventListener('click', clickHandler, false);
+			elements[i].addEventListener("click", clickHandler, false);
 			}
-		}
-	isDown = false;
-	});
+		});
 
-slider.addEventListener('mousemove', (e) => {
-	isDragging = true
-	if(!isDown) return;
-	e.preventDefault();
-	const x = e.pageX - slider.offsetLeft;
-	const walk = x - startX;
-	slider.scrollLeft = scrollLeft - walk;
-	});
+	slider.addEventListener('mouseleave', () => {
+		isDown = false;
+		});
+
+	slider.addEventListener('mouseup', () => {
+		if(isDragging==true){
+			isDragging = false;
+			}
+		else {
+			for (var i = 0; i < elements.length; i++) {
+				elements[i].removeEventListener('click', clickHandler, false);
+				}
+			}
+		isDown = false;
+		});
+
+	slider.addEventListener('mousemove', (e) => {
+		isDragging = true
+		if(!isDown) return;
+		e.preventDefault();
+		const x = e.pageX - slider.offsetLeft;
+		const walk = x - startX;
+		slider.scrollLeft = scrollLeft - walk;
+		});
+}
+
