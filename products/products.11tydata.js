cbdcc118cd32f4654c015d14ea12bbc394718bae
Marco <macio.97.pavanello@gmail.com>
2020-08-19 20:28:54 +0200

    make all icons dynamic

--- products/products.11tydata.js
+++ products/products.11tydata.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 function getFolderFilePaths(folder) {
     paths = [];
@@ -10,7 +11,25 @@ function getFolderFilePaths(folder) {
     return paths;
 }
 
+function getFolderFilePathsWithNames(folder) {
+    paths = [];
+    fs.readdirSync(folder).forEach(file => {
+        let file_path = `${folder}/${file}`;
+        paths.push({
+            "path": file_path,
+            "name": path.parse(file).name
+        });
+    });
+    return paths;
+}
+
 module.exports = {
     "archdec_2": getFolderFilePaths("images/archdec_2/icons"),
-    "archdec": getFolderFilePaths("images/archdec/icons")
-    };
+    "archdec": getFolderFilePaths("images/archdec/icons"),
+    "dielectrics": getFolderFilePathsWithNames("images/materials/dielectrics"),
+    "metals": getFolderFilePathsWithNames("images/materials/metals"),
+    "textures": getFolderFilePathsWithNames("images/materials/textures"),
+    "grass": getFolderFilePathsWithNames("images/grass/grass"),
+    "weeds": getFolderFilePathsWithNames("images/grass/weeds"),
+    "trees": getFolderFilePathsWithNames("images/trees/icons")
+};
