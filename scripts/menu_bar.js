e4d96f07bad73d2a5889daf325e529652f2fe068
macio97 <macio.97.pavanello@gmail.com>
2020-10-17 20:11:21 +0200

    fix menu height, improved collaborators theme support, change coming page to films

--- scripts/menu_bar.js
+++ scripts/menu_bar.js
@@ -1,22 +1,22 @@
 function mouseOver() {
-  if(screen.width >= 600) {
+  if (screen.width >= 600) {
     document.getElementById("section").style.height = "160px";
     document.getElementById("submenu").style.display = "inline-block";
     document.getElementById("submenu").style.opacity = "100%";
-    }
   }
+}
 function mouseOut() {
-  if(screen.width >= 600) {
+  if (screen.width >= 600) {
     document.getElementById("section").style.height = "50px";
     document.getElementById("submenu").style.display = "none";
     document.getElementById("submenu").style.opacity = "0%";
-    }
   }
+}
 function openMenu() {
-	if (document.getElementById("hambmenu").checked) {
-		document.getElementById("section").style.height = "210px";
-	}
-	else {
-		document.getElementById("section").style.height = "50px";
-	}
+  if (document.getElementById("hambmenu").checked) {
+    document.getElementById("section").style.height = "250px";
+  }
+  else {
+    document.getElementById("section").style.height = "50px";
+  }
 }
