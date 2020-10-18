aa17732166b064bdaec8b3118cb5ca034ad80cb2
macio97 <macio.97.pavanello@gmail.com>
2020-10-18 15:33:45 +0200

    add countdown

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,14 +1,21 @@
-const second = 1000,
-			minute = second * 60,
-			hour = minute * 60,
-			day = hour * 24;
+(function () {
+	const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
 
-let countDown = new Date('Mar 17, 2020 16:00:00').getTime(),
-		x = setInterval(function() {
+	let release = "Dec 26, 2020 16:00:00",
+		countDown = new Date(release).getTime(),
+		x = setInterval(function () {
+
 			let now = new Date().getTime(),
-			distance = countDown - now;
-			document.getElementById('days').innerText = Math.floor(distance / (day)),
-			document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-			document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-			document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-		}, second)
+				distance = countDown - now;
+
+			document.getElementById("days").innerText = Math.floor(distance / (day)),
+				document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+				document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+				document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+			//seconds
+		}, 0)
+}());
